a03b2d9da9d5e8257b337cb9b1d1132d8d9bbc19
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-12-06 00:23:07 -0800

    add link

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,8 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = 'data-cache-' + VERSION;
 const FILES_TO_CACHE = [
-    "/",
     "./index.html",
     "./js/idb.js",
     "./js/index.js",
@@ -13,71 +16,49 @@ const FILES_TO_CACHE = [
     "./icons/icon-384x384.png",
     "./icons/icon-512x512.png",
 ];
-  
-  const CACHE_NAME = "static-cache-v1";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
-  self.addEventListener("install", (evt) => {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
     );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (evt) => {
-    // remove old caches
-    evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
+});
+self.addEventListener("fetch", function (e) {
+    if (e.request.url.includes("/api/")) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then(dataCache => {
+                return fetch(e.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            dataCache.put(e.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(e.request);
+                    });
+            })
+            .catch(err => console.log(err))
         );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", (evt) => {
-    // cache successful GET requests to the API
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
-      evt.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then((cache) => {
-            return fetch(evt.request)
-              .then((response) => {
-                // If the response was good, clone it and store it in the cache.
-                if (response.status === 200) {
-                  cache.put(evt.request, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(() => {
-                // Network request failed, try to get it from the cache.
-                return cache.match(evt.request);
-              });
-          })
-          .catch((err) => console.log(err))
-      );
-  
-      // stop execution of the fetch event callback
-      return;
+        return;
     }
-  
-    // if the request is not for the API, serve static assets using
-    // "offline-first" approach.
-    evt.respondWith(
-      caches.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      })
-    );
-  });
-  
+})
